f4090ce11b4d61bb377742212ac18dfaeea2c7b3
lnden <258231674@qq.com>
2019-10-25 10:24:07 +0800

    :ambulance: add static resource

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -36,7 +36,7 @@ class Home extends PureComponent {
             <HomeWrapper>
                 <HomeLeft>
                     <img className="banner-img"
-                         src="//upload.jianshu.io/admin_banners/web_images/4758/0ba87cf2e346d96807631790c073978bbda0bb28.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"
+                         src="./images/banner.png"
                          alt=""/>
                     <List/>
                 </HomeLeft>
